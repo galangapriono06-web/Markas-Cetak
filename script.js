b55cb60f6fb431a761898ec060d9871dcc10278a
galangapriono06-web <galangapriono06@gmail.com>
2026-01-06 13:29:10 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function kirimWA() {
-  alert("Website Markas Cetak Custom siap menerima order!");
+  let nama = document.getElementById('nama').value;
+  let wa = document.getElementById('wa').value;
+  let jenis = document.getElementById('jenis').value;
+  let jumlah = document.getElementById('jumlah').value;
+
+  if (!nama || !wa || !jenis || !jumlah) {
+    alert("Mohon lengkapi semua data!");
+    return;
+  }
+
+  let pesan = `Halo Markas Cetak Custom,
+Saya ingin memesan souvenir.
+
+Nama : ${nama}
+No WA : ${wa}
+Jenis Souvenir : ${jenis}
+Jumlah : ${jumlah}
+
+Mohon info estimasi & konfirmasi.`;
+
+  let admin = '628XXXXXXXXXX'; // GANTI nomor admin kamu
+  window.open(
+    `https://wa.me/${6281215693502}?text=${encodeURIComponent(pesan)}`,
+    '_blank'
+  );
 }
